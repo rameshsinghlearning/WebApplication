218270d43d028885b4ba1c2cfbf1c6e7c883fcb9
Ramesh Singh <ramesh.singh@autofacets.com>
2026-04-05 12:52:59 +0530

    Login Functionality Done

--- StarInfotechMVC/StarInfotechMVC/Controllers/LoginController.cs
+++ StarInfotechMVC/StarInfotechMVC/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StarInfotechMVC.Models;
+using StarInfotechMVC.ViewModels;
 
 namespace StarInfotechMVC.Controllers
 {
@@ -7,6 +9,30 @@ namespace StarInfotechMVC.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dbContext = new AppDbContext();
+
+                var user = dbContext.Users
+                    .FirstOrDefault(x => x.Username == model.Username && x.Password == model.Password);
+
+                if (user == null)
+                {
+                    ViewBag.Message = "Incorrect Username or Password!";
+                    return View(model);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Users");
+                }
+            }
+
+            return View(model);
         }
     }
 }
